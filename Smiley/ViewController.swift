612c8446324c3b2e02b44043cdd70e3a1710bc4b
Amie Kweon <amie.kweon@rd.io>
2015-05-07 17:10:00 -0700

    initial commit

---
+++ Smiley/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smiley
+//
+//  Created by Amie Kweon on 5/7/15.
+//  Copyright (c) 2015 Rdio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
